5b94d421a17b4701cf546eba36adeac1b7e7591e
KayTC <91477747+KayTC@users.noreply.github.com>
2023-07-06 01:27:09 +0700

    Update 06/07/2023 - handle Export Excel

--- resources/views/admin/statistical/details.blade.php
+++ resources/views/admin/statistical/details.blade.php
@@ -6,7 +6,7 @@
             <select class="form-select" aria-label="Default select example" name="cate_export" id="cate_export"> 
                 <option selected value="0">Chọn danh mục</option>
                 @foreach($categoty as $k => $v)
-                    <option value="{{ $v['id'] }}">{{ $v['name'] }}</option>
+                    <option value="{{ $v['id'] }}">{{ ($type_page == 'members')? $v['name_role'] : $v['name']; }}</option>
                 @endforeach
             </select>
             <input type="submit" class="btn btn-success btn-sm" value="Tải xuống tập tin"> 
